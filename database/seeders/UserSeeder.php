b66e96d967622432029651d514ebd4789d2a0033
fole <a_fole@protonmail.com>
2025-07-08 20:39:57 +0100

    Initial setup + DB seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'password',
+        ]);
+
+    User::factory()->count(10)->create();
+    }
+
+}
---
+++ routes/console.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+// Add your custom Artisan commands here
+// Example:
+// Artisan::command('app:example', function () {
+//     $this->info('This is an example command');
+// })->purpose('Example command description');
